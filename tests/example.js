3d81e3c3bc214b16c6d552f3c0c1f658071e6af0
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-12 18:01:06 -0300

    create lib files

--- tests/example.js
+++ tests/example.js
@@ -1,5 +1,7 @@
 const puppeteer =require('puppeteer')
 const expect = require('chai').expect
+const config= require('../lib/config')
+const helpers = require('../lib/helpers')
 
 describe('My first puppeteer test', () =>{
 let browser
@@ -7,23 +9,23 @@ let page
 
 before(async function (){
     browser = await puppeteer.launch({
-        headless:false,
-        slowMo: 0,
-        devtools: false,
-        timeout: 10000,
+        headless:config.isHeadless,
+        slowMo: config.slowMo,
+        devtools: config.isDevtools,
+        timeout: config.waitingTimeout,
     })
     page =await browser.newPage()
-    await page.setDefaultTimeout(10000)
+    await page.setDefaultTimeout(config.launchTimeout)
     await page.setViewport({
-        width:800,
-        height:600
+        width:config.viewportWidth,
+        height:config.viewportHeight
     })
 })
 after(async function(){
 await browser.close()
 })
 it('My first test step', async () =>{
-await page.goto("https://dev.to/")
+await page.goto(config.baseUrl)
 await page.waitForSelector('#nav-search')
 
 const url = await page.url()
@@ -46,8 +48,9 @@ it('browser reload', async () => {
 })
 it('click method', async ()=>{
     await page.goto("https://dev.to/")
-    await page.waitForSelector('#write-link')
-    await page.click('#write-link')
+    //await page.waitForSelector('#write-link')
+    //await page.click('#write-link')
+    await helpers.click(page,'#write-link')
     await page.waitForSelector('.registration-rainbow')
 })
 it('submit searchbox', async ()=>{
